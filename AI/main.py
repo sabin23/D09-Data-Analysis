a1813c8cbc8c82aefea2246f759363a3e8e48cc9
finndr <98969933+finndr@users.noreply.github.com>
2024-03-12 09:57:37 +0100

    Update main.py

--- AI/main.py
+++ AI/main.py
@@ -1,1 +1,20 @@
 import numpy as np
+import scipy.io
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+mat = scipy.io.loadmat("./Data/ae2224I_measurement_data_subj1_C1.mat")
+u = mat['u']
+t = mat['t']
+
+print(u[:,0])
+
+fig, ax = plt.subplots()
+ax.plot(t[0], u[:,0])
+
+ax.set(xlabel='time (s)', ylabel='u',
+       title='u-t')
+ax.grid()
+plt.show()
