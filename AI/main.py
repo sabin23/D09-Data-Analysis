c0cc35f18672067c4e04f23d6d412cfef7caa777
finndr <98969933+finndr@users.noreply.github.com>
2024-03-12 10:57:53 +0000

    ignore

--- AI/main.py
+++ AI/main.py
@@ -1,13 +1,14 @@
 import scipy.io
 import matplotlib.pyplot as plt
-from data_transfer.py import Data 
+from Data\data_transfer.py import Data
 
 
 
 mat = scipy.io.loadmat("./Data/ae2224I_measurement_data_subj1_C1.mat")
 u = mat['u']
 t = mat['t']
-
+data = Data(1,1)
+print(data.u)
 fig, ax = plt.subplots()
 ax.plot(t[0], u[:,0])
 
