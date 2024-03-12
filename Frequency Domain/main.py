fbe4e910054939e571f8bd33e79b57d82cf0bedb
37258 <shvikor@gmail.com>
2024-03-12 10:47:47 +0100

    change

--- Frequency Domain/main.py
+++ Frequency Domain/main.py
@@ -1,10 +1,4 @@
-import numpy as np
+import scipy.io as sp
+mat=sp.loadmat("D:/Downloads/ae2224I_measurement_data_2024 (1)/ae2224I_measurement_data_subj1_C1.mat")
 
-import matplotlib.pyplot as plt
-
-from scipy.fftpack import fft
-
-import scipy as sp
-
-import scipy.io as sio
-
+print(mat)
