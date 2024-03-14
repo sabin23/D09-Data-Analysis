c23cac605ab53c886c3f7568150c4f69978787ca
Mageec8 <2003maciek@gmail.com>
2024-03-14 17:27:01 +0100

    AI_main

--- AI_main.py
+++ AI_main.py
@@ -4,13 +4,8 @@ from get_data import Data
 
 data = Data(1,1)
 print(data.u)
-<<<<<<< HEAD
 fig, ax = plt.subplots()
 ax.plot(data.t[0], data.u[:,0])
-=======
-# fig, ax = plt.subplots()
-# ax.plot(t[0], u[:,0])
->>>>>>> AI_1
 
 # ax.set(xlabel='time (s)', ylabel='u',
 #        title='u-t')
