22a267ca92ed86b6c1845caeaaea16a1d0d10e4b
Mageec8 <2003maciek@gmail.com>
2024-03-14 17:24:00 +0100

    clustering

--- AI/clustering.py
+++ AI/clustering.py
@@ -1,18 +1,29 @@
 import sys
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 # Add the parent directory to sys.path
 sys.path.append("..")
 
 from get_data import Data
 
+data = Data(1, 1)
 
-# data = Data(1, 1)
+u = data.u
+e = data.e
 
-# fig, ax = plt.subplots()
-# ax.plot(data.t[0], data.u[:,0])
+n_clusters = 2
 
-# ax.set(xlabel='time (s)', ylabel='u',
-#        title='u-t')
-# ax.grid()
-# plt.show()
+kmeans = KMeans(n_clusters = n_clusters)
+kmeans.fit(e)
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
+
+plt.scatter(e[:, 0], e[:, 1], c=labels, cmap='viridis', alpha=0.5, label='Data Points')
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='o', s=200, c='red', label='Centroids')
+plt.title('K-means Clustering')
+plt.xlabel('Signal Feature 1')
+plt.ylabel('Signal Feature 2')
+plt.legend()
+plt.show()
--- AI_main.py
+++ AI_main.py
@@ -4,11 +4,11 @@ from get_data import Data
 
 data = Data(1,1)
 print(data.u)
-fig, ax = plt.subplots()
-ax.plot(t[0], u[:,0])
+# fig, ax = plt.subplots()
+# ax.plot(t[0], u[:,0])
 
-ax.set(xlabel='time (s)', ylabel='u',
-       title='u-t')
-ax.grid()
-plt.show()
+# ax.set(xlabel='time (s)', ylabel='u',
+#        title='u-t')
+# ax.grid()
+# plt.show()
 
