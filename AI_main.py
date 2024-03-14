52e0223a957d16041877a9c291a39eb8c43949e2
Mageec8 <2003maciek@gmail.com>
2024-03-14 16:20:50 +0100

    what

--- AI_main.py
+++ AI_main.py
@@ -5,7 +5,7 @@ from get_data import Data
 data = Data(1,1)
 print(data.u)
 fig, ax = plt.subplots()
-ax.plot(t[0], u[:,0])
+ax.plot(data.t[0], data.u[:,0])
 
 ax.set(xlabel='time (s)', ylabel='u',
        title='u-t')
