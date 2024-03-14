a5cb70177efd3c1ba2f32fc8610dc4457db0503a
Mageec8 <2003maciek@gmail.com>
2024-03-14 16:16:10 +0100

    clustering

--- AI/clustering.py
+++ AI/clustering.py
@@ -1,6 +1,18 @@
 import sys
+import matplotlib.pyplot as plt
 
 # Add the parent directory to sys.path
 sys.path.append("..")
 
 from get_data import Data
+
+
+# data = Data(1, 1)
+
+# fig, ax = plt.subplots()
+# ax.plot(data.t[0], data.u[:,0])
+
+# ax.set(xlabel='time (s)', ylabel='u',
+#        title='u-t')
+# ax.grid()
+# plt.show()
