48ac4811855ab93fce0f0ba22713e7dfc618a539
Jelmer12345 <Jelmervandekamp@gmail.com>
2024-03-14 15:06:10 +0100

    ho

--- AI/main.py
+++ AI/main.py
@@ -11,4 +11,10 @@ import scipy.io as sio
 
 mat = sio.loadmat("C:/Users/jelme/Downloads/ae2224I_measurement_data_2024/ae2224I_measurement_data_subj1_C1.mat")
 hpe = mat['Hpe_FC']
-print(hpe)
+
+hper = np.real(hpe)
+hpec = np.imag(hpe)
+
+mag = np.sqrt(np.square(hper)+np.square(hpec))
+
+print(mag)
