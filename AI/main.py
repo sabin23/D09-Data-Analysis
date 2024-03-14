1ec655dd8a630cffca2e16239fff24c623fe280f
Jelmer12345 <Jelmervandekamp@gmail.com>
2024-03-14 16:50:51 +0100

    hi

--- AI/main.py
+++ AI/main.py
@@ -9,7 +9,7 @@ import scipy as sp
 import scipy.io as sio
 
 
-mat = sio.loadmat("C:/Users/jelme/Downloads/ae2224I_measurement_data_2024/ae2224I_measurement_data_subj1_C2.mat")
+mat = sio.loadmat("C:/Users/jelme/Downloads/ae2224I_measurement_data_2024/ae2224I_measurement_data_subj1_C1.mat")
 hpe = mat['Hpe_FC']
 
 hper = np.real(hpe)
