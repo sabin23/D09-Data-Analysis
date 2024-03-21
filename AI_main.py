0da00b149b83ddefaf335aed172a7bf935cc9b90
Mageec8 <2003maciek@gmail.com>
2024-03-21 17:27:32 +0100

    clustering and AI_main

--- AI_main.py
+++ AI_main.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 from get_data import Data
 
 data = Data(1,1)
-print(data.u)
-# fig, ax = plt.subplots()
-# ax.plot(t[0], u[:,0])
+t = data.t
+u = data.u
 
-# ax.set(xlabel='time (s)', ylabel='u',
-#        title='u-t')
-# ax.grid()
-# plt.show()
+fig, ax = plt.subplots()
+ax.plot(t[0], u[:,0])
 
+ax.set(xlabel='time (s)', ylabel='u',
+       title='u-t')
+ax.grid()
+plt.show()
