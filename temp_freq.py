609674b2253b083956aa6acd6a5d39209ac62d52
Power <sabin.ilegitim@gmail.com>
2024-03-21 16:21:42 +0100

    motion vs no motion graphs

--- temp_freq.py
+++ temp_freq.py
@@ -13,7 +13,45 @@ for i in range(1, 7):
     mat.append(line)
 
 
-def plot_Bode():
+def plot_Bode(subject=0, condition=0):
+    a, b = subject, condition
+    # Load a condition
+    ft = mat[a][b].ft
+    u = mat[a][b].u
+    t = mat[a][b].t
+
+
+    # Get magnitude and phase of numbers in Hpe_FC
+    Hpe_FC = mat[a][b].Hpe_FC
+    hper = np.real(Hpe_FC)
+    hpec = np.imag(Hpe_FC)
+    magnitude_Hpe = np.sqrt(np.square(hper) + np.square(hpec))
+    phase_Hpe = np.angle(Hpe_FC, deg=True)
+    # Get magnitude and phase of numbers in Hpxd_FC
+    Hpxd_FC = mat[a][b].Hpxd_FC
+    hpxdr = np.real(Hpxd_FC)
+    hpxdc = np.imag(Hpxd_FC)
+    magnitude_Hpxd = np.sqrt(np.square(hpxdr) + np.square(hpxdc))
+    phase_Hpxd = np.angle(Hpxd_FC, deg=True)
+
+
+    for i in range(len(phase_Hpe)):
+        if i != len(phase_Hpe) - 1:
+            if abs(phase_Hpe[i] - phase_Hpe[i + 1]) >= 180:
+                #for j in range(i + 1, len (phase_Hpe)):
+                phase_Hpe[i + 1] -= 360
+
+    for i in range(len(phase_Hpxd)):
+        if i != len(phase_Hpxd) - 1:
+            if abs(phase_Hpxd[i] - phase_Hpxd[i + 1]) >= 180:
+                #for j in range(i + 1, len (phase_Hpe)):
+                phase_Hpxd[i + 1] -= 360
+                
+            if phase_Hpxd[i] * phase_Hpxd[i + 1] < 0:
+                print(phase_Hpxd[i], phase_Hpxd[i + 1])
+
+
+    w_FC = mat[a][b].w_FC
     # Create a figure with two subplots
     fig, axs = plt.subplots(2)
 
@@ -40,7 +78,13 @@ def plot_Bode():
     plt.tight_layout()
     plt.show()
 
-def plot_FFT():
+def plot_FFT(subject=0, condition=0):
+    a, b = subject, condition
+    # Load a condition
+    ft = mat[a][b].ft
+    u = mat[a][b].u
+    t = mat[a][b].t
+
     # Compute the FFT of both signals
     fft_result_ft = np.fft.fft(ft[:, 0])
     fft_result_u = np.fft.fft(u[:, 0])
@@ -87,56 +131,156 @@ def plot_FFT():
     plt.tight_layout()
     plt.show()
 
-def plot_boxplots():
-    magnitudes = []
-    phases = []
+def plot_6_pilots(condition=0):
+    fig, axs = plt.subplots(2)
+    w_FC = mat[0][condition].w_FC
+
     for i in range(0, 6):
-        for j in range(0, 6):
-            hpe = 0
+        # Get magnitude and phase of numbers in Hpe_FC
+        Hpe_FC = mat[i][condition].Hpe_FC
+        hper = np.real(Hpe_FC)
+        hpec = np.imag(Hpe_FC)
+        magnitude_Hpe = np.sqrt(np.square(hper) + np.square(hpec))
+        phase_Hpe = np.angle(Hpe_FC, deg=True)
+        # Get magnitude and phase of numbers in Hpxd_FC
+        Hpxd_FC = mat[i][condition].Hpxd_FC
+        hpxdr = np.real(Hpxd_FC)
+        hpxdc = np.imag(Hpxd_FC)
+        magnitude_Hpxd = np.sqrt(np.square(hpxdr) + np.square(hpxdc))
+        phase_Hpxd = np.angle(Hpxd_FC, deg=True)
 
-a, b = 4, 5
-# Load a condition
-ft = mat[a][b].ft
-u = mat[a][b].u
-t = mat[a][b].t
+        for j in range(len(phase_Hpe)):
+            if j != len(phase_Hpe) - 1:
+                if abs(phase_Hpe[j] - phase_Hpe[j + 1]) >= 180:
+                    phase_Hpe[j + 1] -= 360
+                if abs(phase_Hpxd[j] - phase_Hpxd[j + 1]) >= 180:
+                    phase_Hpxd[j + 1] -= 360
+        
+        axs[0].semilogx(w_FC, magnitude_Hpe, label=f'Hpe {i + 1}')
+        axs[0].semilogx(w_FC, magnitude_Hpxd, label=f'Hpdx {i + 1}')
+        #axs[0].semilogx(w_FC, (magnitude_Hpe + magnitude_Hpxd) / 2, label='Average')
+        axs[0].set_title('Magnitude of Hpe_FC')
+        axs[0].set_xlabel('Frequency (Hz)')
+        axs[0].set_ylabel('Magnitude')
+        axs[0].grid()
+        axs[0].legend()
 
-# Get magnitude and phase of numbers in Hpe_FC
-Hpe_FC = mat[a][b].Hpe_FC
-hper = np.real(Hpe_FC)
-hpec = np.imag(Hpe_FC)
-magnitude_Hpe = np.sqrt(np.square(hper) + np.square(hpec))
-phase_Hpe = np.angle(Hpe_FC, deg=True)
+        # Plot the phase of Hpe_FC in the second subplot
+        axs[1].semilogx(w_FC, phase_Hpe, label=f'Hpe {i + 1}')
+        axs[1].semilogx(w_FC, phase_Hpxd, label=f'Hpxd {i + 1}')
+        #axs[1].semilogx(w_FC, (phase_Hpe + phase_Hpxd) / 2, label='Average')
+        axs[1].set_title('Phase of Hpe_FC')
+        axs[1].set_xlabel('Frequency (Hz)')
+        axs[1].set_ylabel('Phase (degrees)')
+        axs[1].grid()
+        axs[1].legend()
+    
+    plt.tight_layout()
+    plt.show()
 
-Hpxd_FC = mat[a][b].Hpxd_FC
-hpxdr = np.real(Hpxd_FC)
-hpxdc = np.imag(Hpxd_FC)
-magnitude_Hpxd = np.sqrt(np.square(hpxdr) + np.square(hpxdc))
-phase_Hpxd = np.angle(Hpxd_FC, deg=True)
+def plot_motion_nomotion_average():
+    fig, axs = plt.subplots(2, 2)
+    w_FC = mat[0][0].w_FC
+
+    # First get average magnitude and phase of all pilots in conditions 0-2, only use Hpe
+    for condition in range(0, 3):
+        for pilot in range(0, 6):
+            Hpe_FC = mat[pilot][condition].Hpe_FC
+            hper = np.real(Hpe_FC)
+            hpec = np.imag(Hpe_FC)
+            magnitude_Hpe = np.sqrt(np.square(hper) + np.square(hpec))
+            phase_Hpe = np.angle(Hpe_FC, deg=True)
+            for j in range(len(phase_Hpe)):
+                if j != len(phase_Hpe) - 1:
+                    if abs(phase_Hpe[j] - phase_Hpe[j + 1]) >= 180:
+                        phase_Hpe[j + 1] -= 360
 
 
-for i in range(len(phase_Hpe)):
-    if i != len(phase_Hpe) - 1:
-        if abs(phase_Hpe[i] - phase_Hpe[i + 1]) >= 180:
-            #for j in range(i + 1, len (phase_Hpe)):
-            phase_Hpe[i + 1] -= 360
+            if pilot == 0:
+                magnitude_Hpe_avg = magnitude_Hpe
+                phase_Hpe_avg = phase_Hpe
+            else:
+                magnitude_Hpe_avg += magnitude_Hpe
+                phase_Hpe_avg += phase_Hpe
             
-        if phase_Hpe[i] * phase_Hpe[i + 1] < 0:
-            print(phase_Hpe[i], phase_Hpe[i + 1])
+        average_magnitude_NM = magnitude_Hpe_avg / 6
+        average_phase_NM = phase_Hpe_avg / 6
+        # Plot the average magnitude of Hpe_FC in the first subplot [0, 0], plot average phase in [0, 1] 
+        axs[0, 0].semilogx(w_FC, average_magnitude_NM, label=f'Condition {condition + 1}')
+        axs[0, 1].semilogx(w_FC, average_phase_NM, label=f'Condition {condition + 1}')
 
-for i in range(len(phase_Hpxd)):
-    if i != len(phase_Hpxd) - 1:
-        if abs(phase_Hpxd[i] - phase_Hpxd[i + 1]) >= 180:
-            #for j in range(i + 1, len (phase_Hpe)):
-            phase_Hpxd[i + 1] -= 360
+    # Get average magnitude and phase of all pilots in conditions 3-5, use Hpe and Hpxd
+    for condition in range(3, 6):
+        for pilot in range(0, 6):
+            Hpe_FC = mat[pilot][condition].Hpe_FC
+            hper = np.real(Hpe_FC)
+            hpec = np.imag(Hpe_FC)
+            magnitude_Hpe = np.sqrt(np.square(hper) + np.square(hpec))
+            phase_Hpe = np.angle(Hpe_FC, deg=True)
+            Hpxd_FC = mat[pilot][condition].Hpxd_FC
+            hpxdr = np.real(Hpxd_FC)
+            hpxdc = np.imag(Hpxd_FC)
+            magnitude_Hpxd = np.sqrt(np.square(hpxdr) + np.square(hpxdc))
+            phase_Hpxd = np.angle(Hpxd_FC, deg=True)
+            for j in range(len(phase_Hpe)):
+                if j != len(phase_Hpe) - 1:
+                    if abs(phase_Hpe[j] - phase_Hpe[j + 1]) >= 180:
+                        phase_Hpe[j + 1] -= 360
+                    if abs(phase_Hpxd[j] - phase_Hpxd[j + 1]) >= 180:
+                        phase_Hpxd[j + 1] -= 360
             
-        if phase_Hpxd[i] * phase_Hpxd[i + 1] < 0:
-            print(phase_Hpxd[i], phase_Hpxd[i + 1])
+            if pilot == 0:
+                magnitude_Hpe_avg = magnitude_Hpe
+                magnitude_Hpxd_avg = magnitude_Hpxd
+                phase_Hpe_avg = phase_Hpe
+                phase_Hpxd_avg = phase_Hpxd
+            else:
+                magnitude_Hpe_avg += magnitude_Hpe
+                magnitude_Hpxd_avg += magnitude_Hpxd
+                phase_Hpe_avg += phase_Hpe
+                phase_Hpxd_avg += phase_Hpxd
+            
+        average_magnitude_hpe_M = magnitude_Hpe_avg / 6
+        average_magnitude_hpxd_M = magnitude_Hpxd_avg / 6
+        average_phase_hpe_M = phase_Hpe_avg / 6
+        average_phase_hpxd_M = phase_Hpxd_avg / 6
+        # Plot the average magnitude of Hpe_FC in the first subplot [1, 0], plot average phase in [1, 1] 
+        axs[1, 0].semilogx(w_FC, average_magnitude_hpe_M, label=f'Hpe {condition + 1}', color='red')
+        axs[1, 0].semilogx(w_FC, average_magnitude_hpxd_M, label=f'Hpxd {condition + 1}', color='blue')
+        axs[1, 1].semilogx(w_FC, average_phase_hpe_M, label=f'Hpe {condition + 1}', color='red')
+        axs[1, 1].semilogx(w_FC, average_phase_hpxd_M, label=f'Hpxd {condition + 1}', color='blue')
 
+    axs[0, 0].set_title('Magnitude NM')
+    axs[0, 0].set_xlabel('Frequency (Hz)')
+    axs[0, 0].set_ylabel('Magnitude')
+    axs[0, 0].grid()
+    axs[0, 0].legend()
 
-w_FC = mat[a][b].w_FC
+    axs[0, 1].set_title('Phase NM')
+    axs[0, 1].set_xlabel('Frequency (Hz)')
+    axs[0, 1].set_ylabel('Phase (degrees)')
+    axs[0, 1].grid()
+    axs[0, 1].legend()
+
+    axs[1, 0].set_title('Magnitude M')
+    axs[1, 0].set_xlabel('Frequency (Hz)')
+    axs[1, 0].set_ylabel('Magnitude')
+    axs[1, 0].grid()
+    axs[1, 0].legend()
+
+    axs[1, 1].set_title('Phase M')
+    axs[1, 1].set_xlabel('Frequency (Hz)')
+    axs[1, 1].set_ylabel('Phase (degrees)')
+    axs[1, 1].grid()
+    axs[1, 1].legend()
+
+    plt.tight_layout()
+    plt.show()
 
 frequencies = np.linspace(0, 100, 100)
 #print(frequencies)
 
-
-plot_Bode()
+#plot_Bode(0, 0)
+#plot_FFT(0, 0)
+#plot_6_pilots(0)
+plot_motion_nomotion_average()
