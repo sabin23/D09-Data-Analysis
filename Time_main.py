27cb94a6d2c8c52a6cb9636efdb4431b95b6b469
TurdBurglar69 <W.S.Boer@student.tudelft.nl>
2024-03-26 09:16:33 +0100

    tiddies

--- Time_main.py
+++ Time_main.py
@@ -11,63 +11,17 @@ for i in range(1, 7):
         line.append(Data(i, j))
     mat.append(line)
 
-"""
-ft = mat[2][2].ft
-fd = mat[2][2].fd
-u = mat[2][2].u
-t = mat[2][2].t
-e = mat[2][2].e
-x = mat[2][2].x
-
-print(u)
-
-""" 
 # To do: find variance of each pilot for each run, then find average variance for each run and plot  
 #Compile control and error data for NM case:
-u = mat[1][1].u
-u1 = np.concatenate(u)
-t = mat[1][1].t
 
-def sort_into_test_runs_NM(choice):
+def sort_into_test_runs(choice, a, b):
     u_1 = []
     u_2 = []
     u_3 = []
     u_4 = []
     u_5 = []
     for i in range(1,7):
-        for j in range(1,4):
-            if choice == 'e':
-                matrix = mat[i-1][j-1].e
-            if choice == 'u':
-                matrix = mat[i-1][j-1].u
-            u1 = []   #Creating empty sublists here which lets me put all values per run per pilot
-            u2 = []
-            u3 = []
-            u4 = []
-            u5 = []
-            for value in matrix:
-                u1.append(value[0])
-                u2.append(value[1])
-                u3.append(value[2])
-                u4.append(value[3])
-                u5.append(value[4])
-            u_1.append(np.var(u1)) #These append the variances of the sublists containing signal per run per pilot
-            u_2.append(np.var(u2))
-            u_3.append(np.var(u3))
-            u_4.append(np.var(u4))
-            u_5.append(np.var(u5))
-    
-    runs = [u_1, u_2, u_3, u_4, u_5]
-    return runs
-
-def sort_into_test_runs_M(choice):
-    u_1 = []
-    u_2 = []
-    u_3 = []
-    u_4 = []
-    u_5 = []
-    for i in range(1,7):
-        for j in range(4,7):
+        for j in range(a,b):
             if choice == 'e':
                 matrix = mat[i-1][j-1].e
             if choice == 'u':
@@ -98,16 +52,16 @@ def average_variance_per_run(runs):
         average_variances.append(np.mean(run))
     return average_variances
 
-e_runs_NM = sort_into_test_runs_NM(choice = 'e')
+e_runs_NM = sort_into_test_runs('e',1,4)
 e_average_variances_NM = average_variance_per_run(e_runs_NM)
 
-e_runs_M = sort_into_test_runs_M(choice = 'e')
+e_runs_M = sort_into_test_runs('e',4,7)
 e_average_variances_M = average_variance_per_run(e_runs_M)
 
-u_runs_NM = sort_into_test_runs_NM(choice = 'u')
+u_runs_NM = sort_into_test_runs('u',1,4)
 u_average_variances_NM = average_variance_per_run(u_runs_NM)
 
-u_runs_M = sort_into_test_runs_M(choice = 'u')
+u_runs_M = sort_into_test_runs('u',4,7)
 u_average_variances_M = average_variance_per_run(u_runs_M)
 
 #Checking data
@@ -116,28 +70,67 @@ print(e_average_variances_M)
 print(u_average_variances_NM)
 print(u_average_variances_M)
 
-#Plotting
+
+
+# To do: find variance of each pilot for each run, then find average variance for each run and plot
+
+# Compile control and error data for NM case:
+u_NM = sort_into_test_runs('u',1,4)
+e_NM = sort_into_test_runs('e',1,4)
+
+# Compile control and error data for M case:
+
+u_M = sort_into_test_runs('u',4,7)
+e_M = sort_into_test_runs('e',4,7)
+
+# Calculate variances for each pilot and run in NM case:
+variances_u_NM = []
+variances_e_NM = []
+for i in range(5):
+    variances_u_NM.append([np.var(run) for run in u_NM[i]])
+    variances_e_NM.append([np.var(run) for run in e_NM[i]])
+
+# Calculate variances for each pilot and run in M case:
+variances_u_M = []
+variances_e_M = []
+for i in range(5):
+    variances_u_M.append([np.var(run) for run in u_M[i]])
+    variances_e_M.append([np.var(run) for run in e_M[i]])
+
+# Calculate average variances for each run in NM case:
+average_variances_u_NM = average_variance_per_run(variances_u_NM)
+average_variances_e_NM = average_variance_per_run(variances_e_NM)
+
+# Calculate average variances for each run in M case:
+average_variances_u_M = average_variance_per_run(variances_u_M)
+average_variances_e_M = average_variance_per_run(variances_e_M)
+
+print(len(average_variances_e_NM))
+print(len(average_variances_e_M))
+print(len(average_variances_u_NM))
+print(len(average_variances_u_M))
+
+# Plotting
 run = [1, 2, 3, 4, 5]
-#TODO: Do regression with stats instead of numpy maybe???
-m, b, c, d = np.polyfit(np.array(run), np.array(e_average_variances_NM), 3)
-#m*(np.array(run))**2 + b*np.array(run) + c
-plt.subplot(1,2,1)
+plt.subplot(1, 2, 1)
+plt.ylim(0,13)
 plt.xlabel("Number of runs (1-5)")
-plt.ylabel("Average Error Signal Variance")
+plt.ylabel("Average Error Variance")
 plt.grid()
-plt.scatter(run, e_average_variances_NM, marker = "o")
-plt.plot(np.array(run), m*(np.array(run))**3 + b*(np.array(run))**2 + c*(np.array(run)) + d)
-plt.plot(run, e_average_variances_M, marker = 'x')
-plt.subplot(1,2,2)
-plt.xlabel("Number of runs (1-5)")
-plt.ylabel("Average Control Signal Variance")
-plt.grid()
-plt.plot(run, u_average_variances_NM, marker = 'o')
-plt.plot(run, u_average_variances_M, marker = 'x')
+plt.plot(run, e_average_variances_NM, marker="o", label="NM")
+plt.plot(run, e_average_variances_M, marker="o", label="M")
 
+plt.legend()
+plt.subplot(1, 2, 2)
+plt.ylim(0,13)
+plt.xlabel("Number of runs (1-5)")
+plt.ylabel("Average Control Variance")
+plt.grid()
+plt.plot(run, u_average_variances_NM, marker="o", label="NM")
+plt.plot(run, u_average_variances_M, marker="o", label="M")
+plt.legend()
+plt.savefig('Variance.png')
 plt.show()
-
-
 
 #Code graveyard down here
 """ ***OLD METHOD***
@@ -237,3 +230,71 @@ var_u5 = np.var(u_run5)
 #print(u1)
 #print(t)
 
+# def sort_into_test_runs_M(choice):
+#     u_1 = []
+#     u_2 = []
+#     u_3 = []
+#     u_4 = []
+#     u_5 = []
+#     for i in range(1,7):
+#         for j in range(4,5):
+#             if choice == 'e':
+#                 matrix = mat[i-1][j-1].e
+#             if choice == 'u':
+#                 matrix = mat[i-1][j-1].u
+#             u1 = []   #Creating empty sublists here which lets me put all values per run per pilot
+#             u2 = []
+#             u3 = []
+#             u4 = []
+#             u5 = []
+#             for value in matrix:
+#                 u1.append(value[0])
+#                 u2.append(value[1])
+#                 u3.append(value[2])
+#                 u4.append(value[3])
+#                 u5.append(value[4])
+#             u_1.append(np.var(u1)) #These append the variances of the sublists containing signal per run per pilot
+#             u_2.append(np.var(u2))
+#             u_3.append(np.var(u3))
+#             u_4.append(np.var(u4))
+#             u_5.append(np.var(u5))
+    
+#     runs = [u_1, u_2, u_3, u_4, u_5]
+#     return runs
+
+# u = mat[1][1].u
+# u1 = np.concatenate(u)
+# t = mat[1][1].t
+
+"""
+ft = mat[2][2].ft
+fd = mat[2][2].fd
+u = mat[2][2].u
+t = mat[2][2].t
+e = mat[2][2].e
+x = mat[2][2].x
+
+print(u)
+
+""" 
+
+# #Plotting
+# run = [1, 2, 3, 4, 5]
+# #TODO: Do regression with stats instead of numpy maybe???
+# m, b, c, d = np.polyfit(np.array(run), np.array(e_average_variances_NM), 3)
+# #m*(np.array(run))**2 + b*np.array(run) + c
+# plt.subplot(1,2,1)
+# plt.xlabel("Number of runs (1-5)")
+# plt.ylabel("Average Error Signal Variance")
+# plt.grid()
+# plt.scatter(run, e_average_variances_NM, marker = "o")
+# plt.plot(np.array(run), m*(np.array(run))**3 + b*(np.array(run))**2 + c*(np.array(run)) + d)
+# plt.plot(run, e_average_variances_M, marker = 'x')
+# plt.subplot(1,2,2)
+# plt.xlabel("Number of runs (1-5)")
+# plt.ylabel("Average Control Signal Variance")
+# plt.grid()
+# plt.plot(run, u_average_variances_NM, marker = 'o')
+# plt.plot(run, u_average_variances_M, marker = 'x')
+
+# plt.show()
